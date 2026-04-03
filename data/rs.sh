711ed0b6b77cfdec59310527e4af81f7a37b4d8a
Jinyan <jinyancool@163.com>
2026-04-03 13:21:30 +0800

    embryo

--- data/rs.sh
+++ data/rs.sh
@@ -1,1 +1,4 @@
-rsync -avzP 10.5.0.2:/cluster/home/ztao_jh/projects/embryo/analysis/zhangjing/human/rnaseq/webobj/used/*.rds ./
+#rsync -avzP 10.5.0.2:/cluster/home/ztao_jh/projects/embryo/analysis/zhangjing/human/rnaseq/webobj/used/*.rds ./
+rsync -avzP 10.5.0.2:/cluster/home/ztao_jh/projects/embryo/analysis/zhangjing/human/rnaseq/webobj/used/updata/* ./
+mv *.csv /Users/jhuang/projects/embryo/code/wangwenjie/shinyapps/embryo/data
+mv *.tsv /Users/jhuang/projects/embryo/code/wangwenjie/shinyapps/embryo/data
